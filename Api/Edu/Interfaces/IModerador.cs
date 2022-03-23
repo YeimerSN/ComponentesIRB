135e2441b0d45bda815dd57c63c71f6ba15a0b91
Juan David <jdrosero59@gmail.com>
2022-03-23 18:19:23 -0500

    Milagro

--- Api/Edu/Interfaces/IModerador.cs
+++ Api/Edu/Interfaces/IModerador.cs
@@ -1,7 +1,34 @@
-﻿namespace Api.Edu.Interfaces
+﻿using Api.Edu.Modelo;
+using System.Collections.Generic;
+
+namespace Api.Edu.Interfaces
 {
     public interface IModerador
     {
-        public void moderar();
+        #region Usuarios
+        public void ModerarCrear(Usuario U);
+        public void ModerarEditar(Usuario U);
+        public void ModerarEliminar(Usuario U);
+        public List<Usuario> ModerarLeerU();
+        public List<Persona> ModerarLeerE();
+        public object ModerarLoggearU(int codigo,string password);
+        #endregion
+
+        #region Productos
+        public void ModerarCrear(Libro U);
+        public void ModerarEditar(Libro U);
+        public void ModerarEliminar(Libro U);
+        public List<Libro> ModerarLeerL(Libro U);
+        public void ModerarCrear(Pelicula U);
+        public void ModerarEditar(Pelicula U);
+        public void ModerarEliminar(Pelicula U);
+        public List<Pelicula> ModerarLeerP(Pelicula U);
+        public void ModerarCrear(Genero U);
+        public void ModerarEditar(Genero U);
+        public void ModerarEliminar(Genero U);
+        public List<Genero> ModerarLeerG();
+        public List<Prestamo> ModerarLeerPres();
+        #endregion
+
     }
 }
--- Componentes/Edu/Orquestador.cs
+++ Componentes/Edu/Orquestador.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Api.Edu.Interfaces;
+using Api.Edu.Modelo;
 using Api.Edu.Utilidades;
 
 namespace Componentes.Edu
 {
     public class Orquestador : IModerador
     {
+        private static string rBack=@"C:\Users\Portatil\source\repos\ComponentesIRB\Componentes\Back";
+        private static string rFront= @"C:\Users\Portatil\source\repos\ComponentesIRB\Componentes\Front";
+        private Cargador carBack;
+        private Cargador carFront;
+        private Type type=null;
+        public Orquestador()
+        {
+            carBack = new Cargador(rBack);
+            carFront = new Cargador(rFront);
+        }
+        /*
         public void moderar()
         {
+            
             //string ruta = Directory.GetCurrentDirectory();
             string ruta = @"C:\Users\Portatil\source\repos\ComponentesIRB\Componentes\Front";
             //ruta = Path.Combine(ruta, "Front");
@@ -85,10 +98,154 @@ namespace Componentes.Edu
                 Console.WriteLine(exc);
             }
         }
+        */
         static void Main(string[] args)
         {
-            new Orquestador().moderar();
-            Console.WriteLine("\nFunciona");
+            var o = new Orquestador();
+            try
+            {
+
+            }catch(Exception E)
+            {
+
+            }
+        }
+        
+        public void ModerarCrear(Usuario U)
+        {
+            type = carFront.getClase("Api.Edu.Interfaces.IUsuario");
+            dynamic ent = Activator.CreateInstance(type);
+            ent.agregarUsuario(U);
+            //Añadir un catch en el controlador
+        }
+
+        public void ModerarEditar(Usuario U)
+        {
+            type = carFront.getClase("Api.Edu.Interfaces.IUsuario");
+            dynamic ent = Activator.CreateInstance(type);
+            ent.editarUsuario(U);
+            //Añadir un catch en el controlador
+        }
+
+        public void ModerarEliminar(Usuario U)
+        {
+            type = carFront.getClase("Api.Edu.Interfaces.IUsuario");
+            dynamic ent = Activator.CreateInstance(type);
+            ent.eliminarUsuario(U);
+            //Añadir un catch en el controlador
+        }
+
+        public List<Usuario> ModerarLeerU()
+        {
+            type = carFront.getClase("Api.Edu.Interfaces.IUsuario");
+            dynamic ent = Activator.CreateInstance(type);
+            return ent.getUsuarios();
+            //Añadir un catch en el controlador
+        }
+
+        public List<Persona> ModerarLeerE()
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IEmpleado");
+            dynamic ent = Activator.CreateInstance(type);
+            return ent.GetEmpleados();
+        }
+
+        public object ModerarLoggearU(int codigo, string password)
+        {
+            type = carFront.getClase("Api.Edu.Interfaces.ILogger");
+            dynamic ent = Activator.CreateInstance(type);
+            return ent.loggear(codigo, password);
+        }
+
+        public void ModerarCrear(Libro U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.ILibro");
+            dynamic ent = Activator.CreateInstance(type,U);
+            ent.agregarLibro();
+        }
+
+        public void ModerarEditar(Libro U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.ILibro");
+            dynamic ent = Activator.CreateInstance(type, U);
+            ent.editarLibro();
+        }
+
+        public void ModerarEliminar(Libro U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.ILibro");
+            dynamic ent = Activator.CreateInstance(type, U);
+            ent.eliminarLibro();
+        }
+
+        public List<Libro> ModerarLeerL(Libro U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.ILibro");
+            dynamic ent = Activator.CreateInstance(type, U);
+            return ent.getLibros();
+        }
+
+        public void ModerarCrear(Pelicula U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IPelicula");
+            dynamic ent = Activator.CreateInstance(type, U);
+            ent.agregarPelicula();
+        }
+
+        public void ModerarEditar(Pelicula U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IPelicula");
+            dynamic ent = Activator.CreateInstance(type, U);
+            ent.editarPelicula();
+        }
+
+        public void ModerarEliminar(Pelicula U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IPelicula");
+            dynamic ent = Activator.CreateInstance(type, U);
+            ent.eliminarPelicula();
+        }
+
+        public List<Pelicula> ModerarLeerP(Pelicula U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IPelicula");
+            dynamic ent = Activator.CreateInstance(type);
+            return ent.getPeliculas();
+        }
+
+        public void ModerarCrear(Genero U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IGenero");
+            dynamic ent = Activator.CreateInstance(type);
+            ent.agregarGenero(U);
+        }
+
+        public void ModerarEditar(Genero U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IGenero");
+            dynamic ent = Activator.CreateInstance(type);
+            ent.editarGenero(U);
+        }
+
+        public void ModerarEliminar(Genero U)
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IGenero");
+            dynamic ent = Activator.CreateInstance(type);
+            ent.eliminarGenero(U);
+        }
+
+        public List<Genero> ModerarLeerG()
+        {
+            type = carBack.getClase("Api.Edu.Interfaces.IGenero");
+            dynamic ent = Activator.CreateInstance(type);
+           return ent.getGeneros();
+        }
+
+        public List<Prestamo> ModerarLeerPres()
+        {
+            type = carFront.getClase("Api.Edu.Interfaces.IPrestamo");
+            dynamic ent = Activator.CreateInstance(type);
+            return ent.prestamos();
         }
     }
 }
